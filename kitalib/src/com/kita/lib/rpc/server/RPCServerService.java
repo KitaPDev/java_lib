5e1dd87cc145ee471b3ea3539508d4b7dda4ea2f
KitaPDev <kita.pairojtanachai@gmail.com>
2019-06-25 21:56:02 +0700

    changed RPCServerService to handle Arduino's request

--- kitalib/src/com/kita/lib/rpc/server/RPCServerService.java
+++ kitalib/src/com/kita/lib/rpc/server/RPCServerService.java
@@ -2,9 +2,13 @@ package com.kita.lib.rpc.server;
 
 import com.kita.lib.rpc.BEANRemoteExecution;
 
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class RPCServerService implements Runnable {
 
@@ -17,18 +21,46 @@ public class RPCServerService implements Runnable {
     public void run() {
         try {
             System.out.println("Retrieving socket data . . .");
-            ObjectInputStream serverInputStream = new ObjectInputStream(m_clientSocket.getInputStream());
+//            ObjectInputStream serverInputStream = new ObjectInputStream(m_clientSocket.getInputStream());
+//
+//            BEANRemoteExecution beanRemoteExecution = (BEANRemoteExecution) serverInputStream.readObject();
+//
+//            System.out.println("Executing " + beanRemoteExecution.getMethodName() + " method at " + beanRemoteExecution.getClassName() +
+//                    " class");
+//            Object objReturn = CustomReflection.executeMethod(beanRemoteExecution.getClassName(), beanRemoteExecution.getMethodName(),
+//                    beanRemoteExecution.getMethodParameters());
+//
+//            ObjectOutputStream out = new ObjectOutputStream(m_clientSocket.getOutputStream());
+////            out.writeObject(objReturn);
+////            out.flush();
 
-            BEANRemoteExecution beanRemoteExecution = (BEANRemoteExecution) serverInputStream.readObject();
+            ObjectInputStream ois = new ObjectInputStream(m_clientSocket.getInputStream());
+            String strInputStream = ois.readUTF();
 
+            BEANRemoteExecution beanRemoteExecution = new BEANRemoteExecution();
+            Scanner scanner = new Scanner(strInputStream);
+
+            if(strInputStream.length() > 1) {
+                beanRemoteExecution.setClassName(scanner.next());
+                beanRemoteExecution.setMethodName(scanner.next());
+
+                List lsMethodParameters = new ArrayList();
+                while(scanner.hasNext()) {
+                    lsMethodParameters.add(scanner.next());
+                }
+
+                beanRemoteExecution.setMethodParameters(lsMethodParameters);
+            }
             System.out.println("Executing " + beanRemoteExecution.getMethodName() + " method at " + beanRemoteExecution.getClassName() +
                     " class");
+
             Object objReturn = CustomReflection.executeMethod(beanRemoteExecution.getClassName(), beanRemoteExecution.getMethodName(),
                     beanRemoteExecution.getMethodParameters());
 
             ObjectOutputStream out = new ObjectOutputStream(m_clientSocket.getOutputStream());
             out.writeObject(objReturn);
             out.flush();
+
             System.out.println("Result sent to client");
 
         } catch (Exception e) {
